b8a01b2210ef8cefe0e1386bc48b51604f6966c3
innovate444444 <154489214+innovate444444@users.noreply.github.com>
2024-01-03 19:58:40 +0530

    3 jan 1

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,21 @@
 'use client';
 
-import { Modal } from "@/components/ui/modal";
+import { useEffect } from "react";
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if(!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
     return (
       <div className="p-4">
-        <Modal title="Test" description="Test" isOpen onClose={()=>{}}>
-            Children
-        </Modal>
+        Root
       </div>
     )
   }
--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -1,4 +1,4 @@
-"use-client";
+"use client";
 
 
 import { StoreModal } from "@/components/modals/store-modal";
